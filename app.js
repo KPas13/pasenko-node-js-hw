683d86341bb5c5f376eeb85d1cd21b26ab178244
KPas13 <kpasenko64@gmail.com>
2024-03-05 16:35:41 +0200

    add code for homework 3

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ import logger from './lib/logger/logger.js';
 import {add} from './handler.js';
 
 const log = logger.getLogger('app.js');
+const data = {1: '1'};
+const user = {name: 'Kate', age: 19};
 
 log.info(color);
+log.info("Data", data, "User:", user, "Sdf", "test");
 log.warn(fruit);
 log.error("ERROR occur: My log");
 log.debug(color);
